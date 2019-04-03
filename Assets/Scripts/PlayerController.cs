4f6ff57ddb387b5990ae140a94149f2360da29d7
Sooty Forge <jetttay@hotmail.com>
2019-04-03 23:39:53 +1100

    Joystick Build
    It's Roll-A-Ball.

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed;
+    public Joystick joystick;
+    private Rigidbody rb;
+
+    void Start()
+    {
+        joystick = GameObject.Find("Background").GetComponent<Joystick>();
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate()
+    {
+        
+        float moveHorizontal = joystick.Horizontal;
+        float moveVertical = joystick.Vertical;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
